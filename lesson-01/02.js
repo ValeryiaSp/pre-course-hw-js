462e696bb2103906c71e6c69137642345a242ce9
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2024-05-24 14:34:02 +0300

    feat: add solution of the lesson 1 task 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,1 +1,10 @@
-// code
+let number1 = Math.floor(Math.random() * 100);
+
+function isOddModulus(num) {
+  if (num % 2 === 1) {
+    return num + " : Это нечетное число";
+  }
+  return num + " : Это четное число";
+};
+
+console.log(isOddModulus(number1));

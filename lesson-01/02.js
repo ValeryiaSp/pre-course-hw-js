ee0f367853ae7022ca42161eaa0397488fc98066
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2024-05-24 14:45:51 +0300

    fix: chenge name of variable in function of  the lesson 1 task 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,10 +1,10 @@
 let number1 = Math.floor(Math.random() * 100);
 
-function isOddModulus(num) {
-  if (num % 2 === 1) {
-    return num + " : Это нечетное число";
+function isOddModulus(number1) {
+  if (number1  % 2 === 1) {
+    return number1 + " : Это нечетное число";
   }
-  return num + " : Это четное число";
+  return number1 + " : Это четное число";
 };
 
 console.log(isOddModulus(number1));

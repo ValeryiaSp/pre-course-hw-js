28972e258c73131db34ebaa7e63475589fb31578
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2024-05-24 15:00:57 +0300

    style:fix misprinting in the lesson 1 task 2

--- lesson-01/02.js
+++ lesson-01/02.js
@@ -1,7 +1,7 @@
 let number1 = Math.floor(Math.random() * 100);
 
 function isOddModulus(number1) {
-  if (number1  % 2 === 1) {
+  if (number1 % 2 === 1) {
     return number1 + " : Это нечетное число";
   }
   return number1 + " : Это четное число";

d59620dd05d985672c4da7e98ab04e395920575c
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2024-06-07 11:49:35 +0300

    feat: add solution of the task 2 lesson 3

---
+++ lesson-03/02.js
@@ -0,0 +1,7 @@
+const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
+let indexOfOrange = words.indexOf("апельсин");
+if(indexOfOrange) {
+  console.log ("Ура! нашел")
+} else {
+  console.log("Придется поискать в другом магазине…")
+}

b7f7fa402d2ac7323dac90aa7d4b977a6b740cac
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2024-06-07 16:46:04 +0300

    feat: add solution of the task 7 lesson 3

---
+++ lesson-03/07.js
@@ -0,0 +1,9 @@
+const numbers = [
+  [1, 2, 3, 4, 5],
+  [11, 20, 33, 40, 55],
+  [111, 200, 333, 400, 555],
+];
+
+const filteredNumbers = numbers.map(row => row.filter(item => item % 2 === 0));
+
+console.log(filteredNumbers);

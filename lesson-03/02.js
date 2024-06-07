49801702e0955379c4140bb7a12fbfa72705e864
ValeryiaSp <ValeryiaSpitsyna@yandex.by>
2024-06-07 11:59:06 +0300

    fix: change  solution of the task 2 lesson 3

--- lesson-03/02.js
+++ lesson-03/02.js
@@ -1,8 +1,4 @@
 const words = ["яблоко", "банан", "апельсин", "манго", "киви"];
 let indexOfOrange = words.indexOf("апельсин");
 
-if(indexOfOrange !== -1) {
-  console.log("Ура! нашел");
-} else {
-  console.log("Придется поискать в другом магазине…");
-}
+console.log(indexOfOrange !== -1 ? "Ура! нашел" : "Придется поискать в другом магазине…");
